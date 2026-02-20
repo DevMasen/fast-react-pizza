00950b2c054c7c612f7e54ab2ebc2e95575120ed
DevMasen <mohammadwh400@gmail.com>
2026-02-20 22:09:20 +0330

    E290: feat: search order with id implemented

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,50 +1,18 @@
 // Test ID: IIDSAT
-
-import { useParams } from 'react-router';
+//////////////////
+import { useLoaderData } from 'react-router';
+///////////////////////////////////////////
+import { getOrder } from '../../services/apiRestaurant';
+///////////////////////////////////////////////////////
 import {
 	calcMinutesLeft,
 	formatCurrency,
 	formatDate,
 } from '../../utils/helpers';
-
-const order = {
-	id: 'ABCDEF',
-	customer: 'Jonas',
-	phone: '123456789',
-	address: 'Arroios, Lisbon , Portugal',
-	priority: true,
-	estimatedDelivery: '2027-04-25T10:00:00',
-	cart: [
-		{
-			pizzaId: 7,
-			name: 'Napoli',
-			quantity: 3,
-			unitPrice: 16,
-			totalPrice: 48,
-		},
-		{
-			pizzaId: 5,
-			name: 'Diavola',
-			quantity: 2,
-			unitPrice: 16,
-			totalPrice: 32,
-		},
-		{
-			pizzaId: 3,
-			name: 'Romana',
-			quantity: 1,
-			unitPrice: 15,
-			totalPrice: 15,
-		},
-	],
-	position: '-9.000,38.000',
-	orderPrice: 95,
-	priorityPrice: 19,
-};
-
+//////////////////////////////
 function Order() {
-	const { orderId } = useParams();
-	// Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+	const order = useLoaderData();
+
 	const {
 		id,
 		status,
@@ -59,11 +27,11 @@ function Order() {
 	return (
 		<div>
 			<div>
-				<h2>Status {orderId}</h2>
-
+				<h2>Status</h2>
+				<h3>ID: {id}</h3>
 				<div>
 					{priority && <span>Priority</span>}
-					<span>{status} order</span>
+					<span> {status} order</span>
 				</div>
 			</div>
 
@@ -75,6 +43,12 @@ function Order() {
 				</p>
 				<p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
 			</div>
+
+			<ul>
+				{cart.map(item => (
+					<li key={item.pizzaId}>{item.name}</li>
+				))}
+			</ul>
 
 			<div>
 				<p>Price pizza: {formatCurrency(orderPrice)}</p>
@@ -91,3 +65,8 @@ function Order() {
 }
 
 export default Order;
+
+export async function loader({ params }) {
+	const order = await getOrder(params.orderId);
+	return order;
+}
