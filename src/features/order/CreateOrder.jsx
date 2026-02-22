b518ce319b818e9d4e0a772ad5a3b169b7e94d45
DevMasen <mohammadwh400@gmail.com>
2026-02-22 21:45:05 +0330

    E302: feat: styling buttons hover transition focus ...

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,30 +1,28 @@
-import { createOrder } from "../../services/apiRestaurant";
+import { createOrder } from '../../services/apiRestaurant';
 /////////////////////////////////
-import { Form, redirect, useActionData, useNavigation } from "react-router";
+import { Form, redirect, useActionData, useNavigation } from 'react-router';
 
 const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str,
-  );
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -35,7 +33,7 @@ function CreateOrder() {
   const cart = fakeCart;
   const errors = useActionData();
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
@@ -51,7 +49,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
-          {errors?.phone && <p style={{ color: "red" }}> {errors.phone} </p>}
+          {errors?.phone && <p style={{ color: 'red' }}> {errors.phone} </p>}
         </div>
 
         <div>
@@ -68,8 +66,11 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing your Order..." : "Order Now"}
+          <button
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Placing your Order...' : 'Order Now'}
           </button>
         </div>
       </Form>
@@ -84,18 +85,20 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   const errors = {};
   if (!isValidPhone(order.phone)) {
-    errors.phone = "Please Enter your Phone Number so we can Contact you!";
+    errors.phone = 'Please Enter your Phone Number so we can Contact you!';
     return errors;
   }
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+
+  return null;
 }
 
 export default CreateOrder;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     navigate(`/order/${searchQuery}`);
   }
   return (
-    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b border-stone-500 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast Ract Pizza Co.
       </Link>
