8acd22c2d78c3f1639f66ebb00fc689e71400097
DevMasen <mohammadwh400@gmail.com>
2026-02-23 12:22:36 +0330

    E304: feat: adding reuseable styles in tailwind css

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -41,13 +41,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {errors?.phone && <p style={{ color: 'red' }}> {errors.phone} </p>}
         </div>
@@ -60,7 +60,7 @@ function CreateOrder() {
               type="text"
               name="address"
               required
-              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              className="input min-h-24 rounded-2xl"
             />
           </div>
         </div>
