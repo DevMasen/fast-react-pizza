4e0fae8a2cb5cab841ab45d4d7b36517fc23f6b9
DevMasen <mohammadwh400@gmail.com>
2026-02-23 15:22:37 +0330

    fix: Home header font weight changed

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,8 +2,8 @@ import CreateUser from '../features/user/CreateUser';
 
 function Home() {
   return (
-    <div className="my-10 px-6 text-center sm:my-16">
-      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+    <div className="my-10 px-6 text-center font-semibold sm:my-16">
+      <h1 className="mb-8 text-xl md:text-3xl">
         The best pizza.
         <br />
         <span className="font-normal text-yellow-500">
