87060d8ab9d36c7ffb581776293cc0239ae3679c
DevMasen <mohammadwh400@gmail.com>
2026-02-23 23:30:35 +0330

    E309: feat: cart styles added

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,8 +5,10 @@ function Button({ children, disabled, to, type }) {
     'inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
   const styles = {
-    primary: base + ' px-5 py-3 text-sm md:px-6 md:py-5 md:text-base',
-    small: base + ' px-3 py-2 text-sm md:text-base md:px-3 md:py-3',
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2  md:px-5 md:py-2.5',
+    secondary:
+      'inline-block rounded-full border-2 border-stone-400 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-400 focus:bg-stone-400 focus:outline-none focus:ring focus:ring-stone-400 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5 focus:text-stone-800 hover:text-stone-800',
   };
 
   if (to)
