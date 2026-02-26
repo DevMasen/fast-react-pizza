708d05ea06bd65845e11d13189c6243e281d2833
DevMasen <mohammadwh400@gmail.com>
2026-02-26 12:23:29 +0330

    fix: font family installed and set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Roboto Mono"', ...defaultTheme.fontFamily.sans],
+        mono: ['"Roboto Mono"', 'ui-monospace', 'SFMono-Regular', 'monospace'],
       },
     },
   },
